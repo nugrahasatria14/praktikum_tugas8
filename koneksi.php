1f4a0cd53e2427501fd210fb9910b31e4ff94205
nugrahasatria14 <124239310+nugrahasatria14@users.noreply.github.com>
2025-05-14 11:42:34 +0700

    menambahkan koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,17 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $dbname = "madrasah_db";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function connect() {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->dbname);
+        if ($this->conn->connect_error) {
+            die("Koneksi gagal: " . $this->conn->connect_error);
+        }
+        return $this->conn;
+    }
+}
+?>
